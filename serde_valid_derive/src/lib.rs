fec7e0648baed74603b974f83e9ac36c74daae15
安谷尚人 <yasutani_naoto@uniquevision.co.jp>
2021-02-22 09:42:56 +0900

    add get_strust_fields.

--- serde_valid_derive/src/lib.rs
+++ serde_valid_derive/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro::TokenStream;
 mod validate;
+use proc_macro_error::proc_macro_error;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Validate, attributes(validate))]
+#[proc_macro_error]
 pub fn derive_validate(tokens: TokenStream) -> TokenStream {
     let input = parse_macro_input!(tokens as DeriveInput);
     validate::expand_derive(&input).into()
--- serde_valid_derive/src/validate.rs
+++ serde_valid_derive/src/validate.rs
@@ -1,6 +1,9 @@
+use proc_macro_error::abort;
 use quote::quote;
+use syn::spanned::Spanned;
 
 pub fn expand_derive(input: &syn::DeriveInput) -> proc_macro2::TokenStream {
+    let _struct_fields = get_struct_fields(input);
     let ident = &input.ident;
     let (impl_generics, type_generics, where_clause) = input.generics.split_for_impl();
     let impl_tokens = quote!(
@@ -17,4 +20,26 @@ pub fn expand_derive(input: &syn::DeriveInput) -> proc_macro2::TokenStream {
         }
     );
     impl_tokens
+}
+
+fn get_struct_fields(input: &syn::DeriveInput) -> Vec<&syn::Field> {
+    match input.data {
+        syn::Data::Struct(syn::DataStruct { ref fields, .. }) => fields
+            .iter()
+            .map(|field| {
+                if field.ident.is_none() {
+                    abort!(
+                        fields.span(),
+                        "struct has unnamed fields";
+                        help = "#[derive(Validate)] can only be used on structs with named fields";
+                    );
+                };
+                field
+            })
+            .collect::<Vec<_>>(),
+        _ => abort!(
+            input.span(),
+            "#[derive(Validate)] can only be used with structs"
+        ),
+    }
 }
