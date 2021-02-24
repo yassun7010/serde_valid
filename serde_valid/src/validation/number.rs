691c40bc08e1ae88eebebdf75fb337e4e8a23953
安谷尚人 <yasutani_naoto@uniquevision.co.jp>
2021-02-24 16:27:42 +0900

    add validate_multiples.

--- serde_valid/src/validation/number.rs
+++ serde_valid/src/validation/number.rs
@@ -1,2 +1,4 @@
+mod multiples;
 mod range;
+pub use multiples::validate_multiples;
 pub use range::{validate_range, Limit};
---
+++ serde_valid/src/validation/number/multiples.rs
@@ -0,0 +1,29 @@
+/// MultipleOf validation.
+///
+/// See <https://json-schema.org/understanding-json-schema/reference/numeric.html#id6>
+#[allow(dead_code)]
+pub fn validate_multiples<T>(value: T, multiple_of: T) -> bool
+where
+    T: PartialEq + std::ops::Rem<Output = T> + num_traits::Zero,
+{
+    value % multiple_of == T::zero()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_validate_multiples_generic_ok() {
+        assert!(validate_multiples(10, 5));
+        assert!(validate_multiples(12.0, 1.0));
+        assert!(validate_multiples(12.5, 0.5));
+    }
+
+    #[test]
+    fn test_validate_multiples_generic_fail() {
+        assert!(!validate_multiples(10, 3));
+        assert!(!validate_multiples(12.0, 5.0));
+        assert!(!validate_multiples(12.5, 0.3));
+    }
+}
