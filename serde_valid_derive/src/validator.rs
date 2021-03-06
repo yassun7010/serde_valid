9117f9ad7b9c7444e069897ebd524abde4f2b431
安谷尚人 <yasutani_naoto@uniquevision.co.jp>
2021-03-06 23:31:09 +0900

    rename extract_validator to extract_meta_validator.

--- serde_valid_derive/src/validator.rs
+++ serde_valid_derive/src/validator.rs
@@ -2,8 +2,7 @@ mod meta;
 mod number;
 
 use crate::abort::{abort_invalid_attribute_on_field, abort_unnamed_fields_struct};
-use meta::meta_list::extract_validator_from_meta_list;
-use meta::name_value::extract_validator_from_name_value;
+use meta::extract_meta_validator;
 use proc_macro_error::abort;
 use quote::ToTokens;
 use syn::parse_quote;
@@ -22,7 +21,7 @@ pub fn collect_validators(fields: &syn::Fields) -> Vec<proc_macro2::TokenStream>
             if attribute.path != parse_quote!(validate) {
                 continue;
             }
-            let validator = extract_validator(field_ident, attribute);
+            let validator = extract_meta_validator(field_ident, attribute);
             match validator {
                 Some(validator) => validators.push(validator),
                 None => abort_invalid_attribute_on_field(
@@ -35,49 +34,6 @@ pub fn collect_validators(fields: &syn::Fields) -> Vec<proc_macro2::TokenStream>
     }
 
     validators
-}
-
-fn extract_validator(
-    field_ident: &syn::Ident,
-    attribute: &syn::Attribute,
-) -> Option<proc_macro2::TokenStream> {
-    match attribute.parse_meta() {
-        Ok(syn::Meta::List(syn::MetaList { ref nested, .. })) => {
-            // only validation from there on
-            for meta_item in nested {
-                match meta_item {
-                    syn::NestedMeta::Meta(item) => match item {
-                        syn::Meta::List(meta_list) => {
-                            return extract_validator_from_meta_list(
-                                field_ident,
-                                attribute,
-                                meta_list,
-                            )
-                        }
-                        syn::Meta::NameValue(name_value) => {
-                            return extract_validator_from_name_value(
-                                field_ident,
-                                attribute,
-                                name_value,
-                            )
-                        }
-                        _ => abort!(item.span(), "unsupport non MetaList Meta type"),
-                    },
-                    _ => unreachable!("Found a non Meta while looking for validators"),
-                };
-            }
-        }
-        // TODO
-        Ok(syn::Meta::Path(_)) => abort!(attribute.span(), "Non-support nested arguments"),
-        Ok(syn::Meta::NameValue(_)) => {
-            abort!(attribute.span(), "Unexpected name=value argument")
-        }
-        Err(e) => unreachable!(
-            "Got something other than a list of attributes while checking field `{}`: {:?}",
-            field_ident, e
-        ),
-    };
-    None
 }
 
 #[allow(dead_code)]
--- serde_valid_derive/src/validator/meta.rs
+++ serde_valid_derive/src/validator/meta.rs
@@ -1,2 +1,50 @@
-pub mod meta_list;
-pub mod name_value;
+mod meta_list;
+mod name_value;
+
+use meta_list::extract_validator_from_meta_list;
+use name_value::extract_validator_from_name_value;
+use proc_macro_error::abort;
+use syn::spanned::Spanned;
+
+pub fn extract_meta_validator(
+    field_ident: &syn::Ident,
+    attribute: &syn::Attribute,
+) -> Option<proc_macro2::TokenStream> {
+    match attribute.parse_meta() {
+        Ok(syn::Meta::List(syn::MetaList { ref nested, .. })) => {
+            // only validation from there on
+            for meta_item in nested {
+                match meta_item {
+                    syn::NestedMeta::Meta(item) => match item {
+                        syn::Meta::List(meta_list) => {
+                            return extract_validator_from_meta_list(
+                                field_ident,
+                                attribute,
+                                meta_list,
+                            )
+                        }
+                        syn::Meta::NameValue(name_value) => {
+                            return extract_validator_from_name_value(
+                                field_ident,
+                                attribute,
+                                name_value,
+                            )
+                        }
+                        _ => abort!(item.span(), "unsupport non MetaList Meta type"),
+                    },
+                    _ => unreachable!("Found a non Meta while looking for validators"),
+                };
+            }
+        }
+        // TODO
+        Ok(syn::Meta::Path(_)) => abort!(attribute.span(), "Non-support nested arguments"),
+        Ok(syn::Meta::NameValue(_)) => {
+            abort!(attribute.span(), "Unexpected name=value argument")
+        }
+        Err(e) => unreachable!(
+            "Got something other than a list of attributes while checking field `{}`: {:?}",
+            field_ident, e
+        ),
+    };
+    None
+}
