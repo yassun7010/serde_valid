859a80835761c7d8006d92eaf7a6b66301c312d3
安谷尚人 <yasutani_naoto@uniquevision.co.jp>
2021-03-24 09:24:53 +0900

    add iter method to validation::Errors.

--- serde_valid/src/validation/error/errors.rs
+++ serde_valid/src/validation/error/errors.rs
@@ -1,7 +1,8 @@
 use crate::validation;
-use std::collections::HashMap;
+use std::collections::{hash_map, HashMap};
 
 pub type InnerErrors = HashMap<validation::FieldName, Vec<validation::Error>>;
+pub type InnerErrorsIter<'a> = hash_map::Iter<'a, validation::FieldName, Vec<validation::Error>>;
 
 #[derive(Debug, serde::Serialize, thiserror::Error)]
 pub struct Errors(InnerErrors);
@@ -9,6 +10,10 @@ pub struct Errors(InnerErrors);
 impl Errors {
     pub fn new(errors: InnerErrors) -> Self {
         Self(errors)
+    }
+
+    pub fn iter(&self) -> InnerErrorsIter<'_> {
+        self.0.iter()
     }
 }
 
