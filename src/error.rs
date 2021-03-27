a18bb5ea646f569e26c770c658dcb9004a86de07
安谷尚人 <yasutani_naoto@uniquevision.co.jp>
2021-03-28 02:38:42 +0900

    add error method.

--- src/error.rs
+++ src/error.rs
@@ -16,10 +16,24 @@ impl<E> Error<E>
 where
     E: 'static + std::error::Error,
 {
+    pub fn is_deserialize_error(&self) -> bool {
+        match self {
+            Self::DeserializeError(_) => true,
+            Self::ValidationError(_) => false,
+        }
+    }
+
     pub fn as_deserialize_error(&self) -> Option<&E> {
         match self {
             Self::DeserializeError(error) => Some(error),
             Self::ValidationError(_) => None,
+        }
+    }
+
+    pub fn is_validation_errors(&self) -> bool {
+        match self {
+            Self::DeserializeError(_) => false,
+            Self::ValidationError(_) => true,
         }
     }
 
