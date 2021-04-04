8fffd50037ca7947eb1b26168d9f39ac77eaa088
安谷尚人 <yasutani_naoto@uniquevision.co.jp>
2021-04-04 15:04:14 +0900

    add test.

--- tests/custom_test.rs
+++ tests/custom_test.rs
@@ -20,6 +20,34 @@ fn custom_meta_path_is_ok_test() {
 }
 
 #[test]
+fn custom_meta_list_is_ok_test() {
+    fn user_validation(
+        _val1: &Vec<i32>,
+        _val2: &i32,
+        _lit1: f32,
+        _val3: &f32,
+        _lit2: bool,
+    ) -> Result<(), serde_valid::validation::Error> {
+        Ok(())
+    }
+
+    #[derive(Debug, Validate)]
+    struct TestStruct {
+        #[validate(custom(user_validation(val2, 1.234, val3, true)))]
+        val1: Vec<i32>,
+        val2: i32,
+        val3: f32,
+    }
+
+    let s = TestStruct {
+        val1: vec![1, 2, 3, 4],
+        val2: 5,
+        val3: 1.234,
+    };
+    assert!(s.validate().is_ok());
+}
+
+#[test]
 fn custom_meta_list_when_meta_path_arg_is_ok_test() {
     fn user_validation(
         _val1: &Vec<i32>,
