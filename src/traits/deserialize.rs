f87dc7397d4e15d1f0aa9a9822cfcff8fb39a54e
安谷尚人 <yasutani_naoto@uniquevision.co.jp>
2021-05-01 01:02:58 +0900

    sort module.

--- src/traits/deserialize.rs
+++ src/traits/deserialize.rs
@@ -1,9 +1,9 @@
 #[cfg(not(feature = "serde_error"))]
 mod default_error;
-#[cfg(feature = "serde_error")]
-mod serde_error;
-
 #[cfg(not(feature = "serde_error"))]
 pub use default_error::*;
+
+#[cfg(feature = "serde_error")]
+mod serde_error;
 #[cfg(feature = "serde_error")]
 pub use serde_error::*;
