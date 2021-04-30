13fef8f2a11cc9eafaeefdffaff1db7f95097b46
安谷尚人 <yasutani_naoto@uniquevision.co.jp>
2021-05-01 00:44:52 +0900

    remove other crate helper.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
 mod deserialize;
-#[cfg(not(serde_error))]
-mod error;
 mod traits;
 pub mod validation;
+
 pub use deserialize::*;
-#[cfg(not(serde_error))]
-pub use error::Error;
 pub use traits::*;
 pub use validation::{
     validate_array_items, validate_array_unique_items, validate_generic_enumerate,
@@ -13,39 +10,13 @@ pub use validation::{
     validate_string_length, validate_string_pattern, Limit,
 };
 
+#[cfg(not(serde_error))]
+mod error;
+#[cfg(not(serde_error))]
+pub use error::Error;
+
 pub trait Validate {
     fn validate(&self) -> Result<(), self::validation::Errors>;
 }
 
 pub use serde_valid_derive::Validate;
-
-pub mod json {
-    pub use serde_json::{
-        json, Deserializer, Error, Map, Number, Serializer, StreamDeserializer, Value,
-    };
-}
-
-#[cfg(not(feature = "serde_error"))]
-pub type JsonError = Error<serde_json::Value>;
-
-#[cfg(feature = "yaml")]
-pub mod yaml {
-    pub use serde_yaml::{mapping, Error, Index, Location, Mapping, Number, Sequence, Value};
-}
-
-#[cfg(all(feature = "yaml", not(feature = "serde_error")))]
-pub type YamlError = Error<serde_yaml::Value>;
-
-#[cfg(feature = "toml")]
-pub mod toml {
-    pub use serde_toml::{map, toml, value, Deserializer, Serializer, Value};
-    mod ser {
-        pub use serde_toml::ser::{Error, Serializer};
-    }
-    mod de {
-        pub use serde_toml::de::{Deserializer, Error};
-    }
-}
-
-#[cfg(all(feature = "toml", not(feature = "serde_error")))]
-pub type TomlError = Error<serde_toml::Value>;
