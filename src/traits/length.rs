ebc8a15ef642e2cd9bc96701cc726a3c9f16c4fa
Yasutanium <yassun4dev@outlook.com>
2022-05-04 01:52:10 +0900

    Update: add macro.

--- src/traits/length.rs
+++ src/traits/length.rs
@@ -4,98 +4,71 @@ pub trait Length {
     fn length(&self) -> usize;
 }
 
-impl Length for str {
-    fn length(&self) -> usize {
-        self.graphemes(true).count()
-    }
+macro_rules! impl_for_str {
+    ($ty:ty) => {
+        impl Length for $ty {
+            fn length(&self) -> usize {
+                self.graphemes(true).count()
+            }
+        }
+    };
 }
 
-impl Length for &str {
-    fn length(&self) -> usize {
-        self.graphemes(true).count()
-    }
+impl_for_str!(str);
+impl_for_str!(&str);
+impl_for_str!(String);
+impl_for_str!(std::borrow::Cow<'_, str>);
+
+macro_rules! impl_for_os_str {
+    ($ty:ty) => {
+        impl Length for $ty {
+            fn length(&self) -> usize {
+                self.to_string_lossy().length()
+            }
+        }
+    };
 }
 
-impl Length for String {
-    fn length(&self) -> usize {
-        self.graphemes(true).count()
-    }
+impl_for_os_str!(std::ffi::OsStr);
+impl_for_os_str!(&std::ffi::OsStr);
+impl_for_os_str!(std::ffi::OsString);
+impl_for_os_str!(std::borrow::Cow<'_, std::ffi::OsStr>);
+
+macro_rules! impl_for_path {
+    ($ty:ty) => {
+        impl Length for $ty {
+            fn length(&self) -> usize {
+                self.as_os_str().length()
+            }
+        }
+    };
 }
 
-impl Length for std::borrow::Cow<'_, str> {
-    fn length(&self) -> usize {
-        self.graphemes(true).count()
-    }
+impl_for_path!(std::path::Path);
+impl_for_path!(&std::path::Path);
+impl_for_path!(std::path::PathBuf);
+impl_for_os_str!(std::borrow::Cow<'_, std::path::Path>);
+
+macro_rules! impl_for_bytes {
+    ($ty:ty) => {
+        impl Length for $ty {
+            fn length(&self) -> usize {
+                self.len()
+            }
+        }
+    };
+    ($ty:ty, $size:tt) => {
+        impl<const $size: usize> Length for [$ty; $size] {
+            fn length(&self) -> usize {
+                self.len()
+            }
+        }
+    };
 }
 
-impl Length for Vec<u8> {
-    fn length(&self) -> usize {
-        self.len()
-    }
-}
-
-impl Length for Vec<char> {
-    fn length(&self) -> usize {
-        self.len()
-    }
-}
-
-impl Length for [u8] {
-    fn length(&self) -> usize {
-        self.len()
-    }
-}
-
-impl<const N: usize> Length for [u8; N] {
-    fn length(&self) -> usize {
-        self.len()
-    }
-}
-
-impl Length for [char] {
-    fn length(&self) -> usize {
-        self.len()
-    }
-}
-
-impl<const N: usize> Length for [char; N] {
-    fn length(&self) -> usize {
-        self.len()
-    }
-}
-
-impl Length for std::ffi::OsStr {
-    fn length(&self) -> usize {
-        self.to_string_lossy().length()
-    }
-}
-
-impl Length for &std::ffi::OsStr {
-    fn length(&self) -> usize {
-        self.to_string_lossy().length()
-    }
-}
-
-impl Length for std::ffi::OsString {
-    fn length(&self) -> usize {
-        self.as_os_str().length()
-    }
-}
-
-impl Length for std::path::Path {
-    fn length(&self) -> usize {
-        self.as_os_str().length()
-    }
-}
-
-impl Length for &std::path::Path {
-    fn length(&self) -> usize {
-        self.as_os_str().length()
-    }
-}
-
-impl Length for std::path::PathBuf {
-    fn length(&self) -> usize {
-        self.as_os_str().length()
-    }
-}
+impl_for_bytes!(Vec<u8>);
+impl_for_bytes!(Vec<char>);
+impl_for_bytes!([u8]);
+impl_for_bytes!([char]);
+impl_for_bytes!(u8, N);
+impl_for_bytes!(char, N);
