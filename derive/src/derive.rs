a288c318ba48a6f317c8d0b3be8f69ec781b3988
Yasutanium <yassun4dev@outlook.com>
2022-05-03 13:32:28 +0900

    WIP: add result type.

--- derive/src/derive.rs
+++ derive/src/derive.rs
@@ -9,18 +9,18 @@ use proc_macro_error::abort;
 use syn::spanned::Spanned;
 use unnamed_struct_derive::expand_unnamed_struct_derive;
 
-pub fn expand_derive(input: &syn::DeriveInput) -> TokenStream {
+pub fn expand_derive(input: &syn::DeriveInput) -> Result<TokenStream, Vec<syn::Error>> {
     match &input.data {
         syn::Data::Struct(syn::DataStruct { ref fields, .. }) => match fields {
-            syn::Fields::Named(named) => expand_named_struct_derive(input, named),
-            syn::Fields::Unnamed(unnamed) => expand_unnamed_struct_derive(input, unnamed),
+            syn::Fields::Named(named) => Ok(expand_named_struct_derive(input, named)),
+            syn::Fields::Unnamed(unnamed) => Ok(expand_unnamed_struct_derive(input, unnamed)),
             syn::Fields::Unit => abort!(
                 input.span(),
                 "#[derive(Validate)] not support unit field struct"
             ),
         },
         syn::Data::Enum(syn::DataEnum { variants, .. }) => {
-            expand_enum_validate_derive(input, variants)
+            Ok(expand_enum_validate_derive(input, variants))
         }
         syn::Data::Union(_) => abort!(input.span(), "#[derive(Validate)] not support union data"),
     }
--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -8,11 +8,20 @@ mod validator;
 
 use derive::expand_derive;
 use proc_macro_error::proc_macro_error;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Validate, attributes(validate))]
 #[proc_macro_error]
 pub fn derive_validate(tokens: TokenStream) -> TokenStream {
     let input = parse_macro_input!(tokens as DeriveInput);
-    expand_derive(&input).into()
+
+    expand_derive(&input)
+        .unwrap_or_else(to_compile_errors)
+        .into()
+}
+
+fn to_compile_errors(errors: Vec<syn::Error>) -> proc_macro2::TokenStream {
+    let compile_errors = errors.iter().map(syn::Error::to_compile_error);
+    quote!(#(#compile_errors)*)
 }
