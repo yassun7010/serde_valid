b105c67294204c84654df6b5575594de827af179
yassun4dev <yassun4dev@outlook.com>
2022-05-05 23:18:15 +0900

    Update: remove module.

--- derive/src/lit.rs
+++ derive/src/lit.rs
@@ -1,2 +1,16 @@
-mod numeric;
-pub use numeric::LitNumeric;
+use proc_macro2::TokenStream;
+use quote::ToTokens;
+
+pub enum LitNumeric<'a> {
+    Int(&'a syn::LitInt),
+    Float(&'a syn::LitFloat),
+}
+
+impl<'a> ToTokens for LitNumeric<'a> {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        match self {
+            LitNumeric::Int(lin) => lin.to_tokens(tokens),
+            LitNumeric::Float(lin) => lin.to_tokens(tokens),
+        }
+    }
+}
