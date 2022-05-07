88fbbc2ad9c00dea781152e5e45e382d9dd3ea4f
Yasutanium <yassun4dev@outlook.com>
2022-05-08 01:21:08 +0900

    Fix: test case.

--- tests/enum_test.rs
+++ tests/enum_test.rs
@@ -18,19 +18,19 @@ fn enum_named_variant_validation_is_ok() {
     }
 
     let s = TestEnum::Named {
-        a: TestStruct { val: 5 },
+        a: TestStruct { val: 12 },
     };
-    assert!(s.validate().is_ok());
+    assert!(s.validate().is_err());
 }
 
 #[test]
 fn enum_unnamed_variant_validation_is_ok() {
     #[derive(Validate)]
     enum TestEnum {
-        #[allow(dead_code)]
-        NewType(#[validate(range(minimum = 0, maximum = 10))] i32),
         UnNamed(
-            #[validate(range(minimum = 0, maximum = 10))] i32,
+            #[validate(minimum = 0)]
+            #[validate(maximum = 10)]
+            i32,
             #[validate] TestStruct,
         ),
     }
@@ -50,21 +50,13 @@ fn enum_unnamed_variant_validation_is_ok() {
 fn enum_newtype_variant_validation_is_ok() {
     #[derive(Validate)]
     enum TestEnum {
-        NewType(#[validate(range(minimum = 0, maximum = 10))] i32),
-        #[allow(dead_code)]
-        UnNamed(
-            #[validate(range(minimum = 0, maximum = 10))] i32,
-            #[validate] TestStruct,
+        NewType(
+            #[validate(minimum = 0)]
+            #[validate(maximum = 10)]
+            i32,
         ),
     }
 
-    #[derive(Validate)]
-    struct TestStruct {
-        #[validate(minimum = 0)]
-        #[validate(maximum = 10)]
-        val: i32,
-    }
-
-    let s = TestEnum::NewType(5);
-    assert!(s.validate().is_ok());
+    let s = TestEnum::NewType(15);
+    assert!(s.validate().is_err());
 }
