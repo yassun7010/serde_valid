b4855ad740b217414a0b5899d3c35dc725f92304
Yasutanium <yassun4dev@outlook.com>
2022-05-08 12:17:03 +0900

    Add: test case.

--- tests/rule_test.rs
+++ tests/rule_test.rs
@@ -1,13 +1,20 @@
+use serde_json::json;
 use serde_valid::Validate;
 
-fn sample_rule(_val: &i32) -> Result<(), serde_valid::validation::Error> {
+fn sample_ok_rule(_val: &i32) -> Result<(), serde_valid::validation::Error> {
     Ok(())
+}
+
+fn sample_err_rule(_val: &i32) -> Result<(), serde_valid::validation::Error> {
+    Err(serde_valid::validation::Error::Custom(
+        "rule error.".to_owned(),
+    ))
 }
 
 #[test]
 fn rule_struct_named_fields_is_ok() {
     #[derive(Validate)]
-    #[rule(sample_rule(val))]
+    #[rule(sample_ok_rule(val))]
     struct TestStruct {
         val: i32,
     }
@@ -17,9 +24,29 @@ fn rule_struct_named_fields_is_ok() {
 }
 
 #[test]
+fn rule_struct_named_fields_is_err() {
+    #[derive(Validate)]
+    #[rule(sample_err_rule(val))]
+    struct TestStruct {
+        val: i32,
+    }
+
+    let s = TestStruct { val: 5 };
+    assert_eq!(
+        serde_json::to_string(&s.validate().unwrap_err()).unwrap(),
+        serde_json::to_string(&json!({
+            "val": [
+                "rule error."
+            ]
+        }))
+        .unwrap()
+    );
+}
+
+#[test]
 fn rule_struct_unnamed_fields_is_ok() {
     #[derive(Validate)]
-    #[rule(sample_rule(0))]
+    #[rule(sample_ok_rule(0))]
     struct TestStruct(i32, i32);
 
     let s = TestStruct(5, 5);
@@ -29,7 +56,7 @@ fn rule_struct_unnamed_fields_is_ok() {
 #[test]
 fn rule_new_type_is_ok() {
     #[derive(Validate)]
-    #[rule(sample_rule(0))]
+    #[rule(sample_ok_rule(0))]
     struct TestNewType(i32);
 
     let s = TestNewType(5);
@@ -40,13 +67,13 @@ fn rule_new_type_is_ok() {
 fn rule_enum_is_ok() {
     #[derive(Validate)]
     enum TestEnum {
-        #[rule(sample_rule(val))]
+        #[rule(sample_ok_rule(val))]
         NamedFields {
             val: i32,
         },
-        #[rule(sample_rule(0))]
+        #[rule(sample_ok_rule(0))]
         UnnamedFields(i32, i32),
-        #[rule(sample_rule(0))]
+        #[rule(sample_ok_rule(0))]
         NewType(i32),
         NoField,
     }
