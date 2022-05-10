24807a5cd4cdbc7c9a9e9ca6f0b36318f3cdae16
Yasutanium <yassun4dev@outlook.com>
2022-05-10 23:36:23 +0900

    Update: README.

--- src/lib.rs
+++ src/lib.rs
@@ -40,26 +40,26 @@
 //!
 //! Serde Valid support standard validation based JSON Schema.
 //!
-//! | Type | Serde Valid(validate derive)  | Serde Valid (validate trait) | Json Schema |
-//! | :---: | :--- |:--- | :--- |
-//! | String | `#[validate(max_length = 5)]` | `ValidateMaxLength` | [maxLength](https://json-schema.org/understanding-json-schema/reference/string.html#length) |
-//! | String | `#[validate(min_length = 5)]` | `ValidateMinLength` | [minLength](https://json-schema.org/understanding-json-schema/reference/string.html#length) |
-//! | String | `#[validate(pattern = r"^\d{5}$")]` | `ValidatePattern` | [pattern](https://json-schema.org/understanding-json-schema/reference/string.html#regular-expressions) |
-//! | Numeric | `#[validate(maximum = 5)]` | `ValidateMaximum` | [maximum](https://json-schema.org/understanding-json-schema/reference/numeric.html#range) |
-//! | Numeric | `#[validate(minimum = 5)]` | `ValidateMinimum` | [minimum](https://json-schema.org/understanding-json-schema/reference/numeric.html#range) |
-//! | Numeric | `#[validate(exclusive_maximum = 5)]` | `ValidateExclusiveMaximum` | [exclusiveMaximum](https://json-schema.org/understanding-json-schema/reference/numeric.html#range) |
-//! | Numeric | `#[validate(exclusive_minimum = 5)]` | `ValidateExclusiveMinimum` | [exclusiveMinimum](https://json-schema.org/understanding-json-schema/reference/numeric.html#range) |
-//! | Numeric | `#[validate(multiple_of = 5)]` | `ValidateMultipleOf` | [multipleOf](https://json-schema.org/understanding-json-schema/reference/numeric.html#multiples) |
-//! | Object | `#[validate(max_properties = 5)]` | `ValidateMaxProperties` | [maxProperties](https://json-schema.org/understanding-json-schema/reference/object.html#size) |
-//! | Object | `#[validate(min_properties = 5)]` | `ValidateMinProperties` | [minProperties](https://json-schema.org/understanding-json-schema/reference/object.html#size) |
-//! | Array | `#[validate(max_items = 5)]` | `ValidateMaxItems` | [maxItems](https://json-schema.org/understanding-json-schema/reference/array.html#length) |
-//! | Array | `#[validate(min_items = 5)]` | `ValidateMinItems` | [minItems](https://json-schema.org/understanding-json-schema/reference/array.html#length) |
-//! | Array | `#[validate(unique_items)]` | `ValidateUniqueItems` | [uniqueItems](https://json-schema.org/understanding-json-schema/reference/array.html#unique_items) |
-//! | Generic | `#[validate(enumerate(5, 10, 15))]` | `ValidateEnumerate` | [enum](https://json-schema.org/understanding-json-schema/reference/generic.html#enumerated-values) |
+//! | Type | Serde Valid(validate derive) | Json Schema |
+//! | :---: | :--- | :--- |
+//! | String | `#[validate(max_length = 5)]` | [maxLength](https://json-schema.org/understanding-json-schema/reference/string.html#length) |
+//! | String | `#[validate(min_length = 5)]` | [minLength](https://json-schema.org/understanding-json-schema/reference/string.html#length) |
+//! | String | `#[validate(pattern = r"^\d{5}$")]` | [pattern](https://json-schema.org/understanding-json-schema/reference/string.html#regular-expressions) |
+//! | Numeric | `#[validate(maximum = 5)]` | [maximum](https://json-schema.org/understanding-json-schema/reference/numeric.html#range) |
+//! | Numeric | `#[validate(minimum = 5)]` | [minimum](https://json-schema.org/understanding-json-schema/reference/numeric.html#range) |
+//! | Numeric | `#[validate(exclusive_maximum = 5)]` | [exclusiveMaximum](https://json-schema.org/understanding-json-schema/reference/numeric.html#range) |
+//! | Numeric | `#[validate(exclusive_minimum = 5)]` | [exclusiveMinimum](https://json-schema.org/understanding-json-schema/reference/numeric.html#range) |
+//! | Numeric | `#[validate(multiple_of = 5)]` | [multipleOf](https://json-schema.org/understanding-json-schema/reference/numeric.html#multiples) |
+//! | Object | `#[validate(max_properties = 5)]` | [maxProperties](https://json-schema.org/understanding-json-schema/reference/object.html#size) |
+//! | Object | `#[validate(min_properties = 5)]` | [minProperties](https://json-schema.org/understanding-json-schema/reference/object.html#size) |
+//! | Array | `#[validate(max_items = 5)]` | [maxItems](https://json-schema.org/understanding-json-schema/reference/array.html#length) |
+//! | Array | `#[validate(min_items = 5)]` | [minItems](https://json-schema.org/understanding-json-schema/reference/array.html#length) |
+//! | Array | `#[validate(unique_items)]` | [uniqueItems](https://json-schema.org/understanding-json-schema/reference/array.html#unique_items) |
+//! | Generic | `#[validate(enumerate(5, 10, 15))]` | [enum](https://json-schema.org/understanding-json-schema/reference/generic.html#enumerated-values) |
 //!
 //! ## Complete Constructor (Deserialization)
 //!
-//! Serde Valid support complete constructor method using by [`serde_valid::json::FromJson`][FromJson] trait.
+//! Serde Valid support complete constructor method using by [`serde_valid::json::FromJson`][json::FromJson] trait.
 //!
 //! ```rust
 //! use serde::Deserialize;
@@ -85,7 +85,7 @@
 //!
 //! ## Serialization
 //!
-//! For serialization, provides [`serde_valid::json::ToJson`](ToJson) trait.
+//! For serialization, provides [`serde_valid::json::ToJson`](json::ToJson) trait.
 //!
 //! ```rust
 //! use serde::Serialize;
