bb58704e9b2c2c2058673f7bddaabe33e191e4f1
Yasutanium <yassun4dev@outlook.com>
2022-07-11 00:16:53 +0900

    Add: range into_error.

--- src/validation/error.rs
+++ src/validation/error.rs
@@ -14,6 +14,7 @@ pub use error::Error;
 pub use errors::Errors;
 use indexmap::IndexMap;
 pub use object_errors::ObjectErrors;
+use paste::paste;
 
 pub type VecErrors = Vec<Error>;
 pub type MapErrors = IndexMap<&'static str, VecErrors>;
@@ -29,21 +30,36 @@ pub trait IntoError {
     fn into_error(self, format_fn: fn(&Self::Params) -> String) -> Error;
 }
 
-impl IntoError for Composited<EnumerateErrorParams> {
-    type Params = EnumerateErrorParams;
-    fn into_error(self, format_fn: fn(&Self::Params) -> String) -> Error {
-        match self {
-            Composited::Single(single) => Error::Enumerate(Message::new(single, format_fn)),
-            Composited::Array(array) => Error::Items(ArrayErrors::new(
-                Vec::with_capacity(0),
-                array
-                    .into_iter()
-                    .enumerate()
-                    .map(|(index, params)| {
-                        (index, Errors::NewType(vec![params.into_error(format_fn)]))
-                    })
-                    .collect::<IndexMap<_, _>>(),
-            )),
+macro_rules! impl_into_error {
+    ($ErrorType:ident) => {
+        paste! {
+            impl IntoError for Composited<[<$ErrorType ErrorParams>]> {
+                type Params = [<$ErrorType ErrorParams>];
+                fn into_error(self, format_fn: fn(&Self::Params) -> String) -> Error {
+                    match self {
+                        Composited::Single(single) => Error::$ErrorType(Message::new(single, format_fn)),
+                        Composited::Array(array) => Error::Items(ArrayErrors::new(
+                            Vec::with_capacity(0),
+                            array
+                                .into_iter()
+                                .enumerate()
+                                .map(|(index, params)| {
+                                    (index, Errors::NewType(vec![params.into_error(format_fn)]))
+                                })
+                                .collect::<IndexMap<_, _>>(),
+                        )),
+                    }
+                }
+            }
         }
-    }
+    };
 }
+
+// Global
+impl_into_error!(Enumerate);
+
+// Numeric
+impl_into_error!(Maximum);
+impl_into_error!(Minimum);
+impl_into_error!(ExclusiveMaximum);
+impl_into_error!(ExclusiveMinimum);
