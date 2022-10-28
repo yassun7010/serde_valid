38e9637c397d5a7259702aa557f5b507a9c9c256
yassun4dev <yassun4dev@outlook.com>
2022-10-29 01:47:55 +0900

    Update: change FlatErrors serialize.

--- serde_valid/src/flatten/flat_errors.rs
+++ serde_valid/src/flatten/flat_errors.rs
@@ -1,8 +1,8 @@
-use serde::Serialize;
+use serde::ser::SerializeStruct;
 
 use super::FlatError;
 
-#[derive(Debug, PartialEq, Eq, Serialize)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct FlatErrors(Vec<FlatError>);
 
 impl FlatErrors {
@@ -12,6 +12,17 @@ impl FlatErrors {
 
     pub fn errors(&self) -> &[FlatError] {
         &self.0
+    }
+}
+
+impl serde::Serialize for FlatErrors {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut flat_errors = serializer.serialize_struct("FlatErrors", 1)?;
+        flat_errors.serialize_field("errors", &self.0)?;
+        flat_errors.end()
     }
 }
 
@@ -30,5 +41,43 @@ impl<'a> IntoIterator for &'a FlatErrors {
 
     fn into_iter(self) -> Self::IntoIter {
         self.0.iter()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use indexmap::indexmap;
+    use serde_json::json;
+
+    use crate::{
+        error::{Message, ToDefaultMessage},
+        flatten::IntoFlat,
+        validation::{ArrayErrors, Error, Errors},
+        MinItemsError,
+    };
+
+    #[test]
+    fn flat_errors_json() {
+        assert_eq!(
+            serde_json::to_value(
+                Errors::Array(ArrayErrors {
+                    errors: vec![Error::MinItems(Message::new(
+                        MinItemsError { min_items: 1 },
+                        MinItemsError::to_default_message,
+                    ))],
+                    items: indexmap! {},
+                })
+                .into_flat()
+            )
+            .unwrap(),
+            json!({
+                "errors": [
+                    {
+                        "path": "",
+                        "message": "The length of the items must be `>= 1`."
+                    }
+                ]
+            })
+        );
     }
 }
