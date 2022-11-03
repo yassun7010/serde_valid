b3da9803f54bc0c4cb4bb48ea8c8075938646ccc
yassun4dev <yassun4dev@outlook.com>
2022-11-03 19:33:16 +0900

    Add: OperationOutput trait to Rejection.

--- axum_serde_valid/src/rejection.rs
+++ axum_serde_valid/src/rejection.rs
@@ -1,9 +1,11 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, ops::Deref};
 
 use axum::{extract::rejection::JsonRejection, http::StatusCode, response::IntoResponse};
-use jsonschema::{
-    output::{ErrorDescription, OutputUnit},
-    paths::JSONPointer,
+use jsonschema::output::{ErrorDescription, OutputUnit};
+use schemars::{
+    gen::SchemaGenerator,
+    schema::{InstanceType, Schema, SchemaObject},
+    JsonSchema,
 };
 use serde::Serialize;
 use serde_valid::flatten::IntoFlat;
@@ -21,15 +23,41 @@ pub enum Rejection {
     SerdeValid(serde_valid::validation::Errors),
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, JsonSchema)]
 pub struct ErrorResponse {
     errors: Vec<Error>,
 }
 
+#[derive(Debug, Default, Serialize)]
+pub struct JsonPointer(jsonschema::paths::JSONPointer);
+
+impl Deref for JsonPointer {
+    type Target = jsonschema::paths::JSONPointer;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl JsonSchema for JsonPointer {
+    fn schema_name() -> String {
+        "string".to_owned()
+    }
+
+    fn json_schema(_: &mut SchemaGenerator) -> Schema {
+        SchemaObject {
+            instance_type: Some(InstanceType::String.into()),
+            format: None,
+            ..Default::default()
+        }
+        .into()
+    }
+}
+
 /// The response that is returned by default.
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, JsonSchema)]
 pub struct Error {
-    pub path: JSONPointer,
+    pub path: JsonPointer,
     pub message: String,
 }
 
@@ -38,13 +66,13 @@ impl From<Rejection> for ErrorResponse {
         match rejection {
             Rejection::Json(v) => Self {
                 errors: vec![Error {
-                    path: JSONPointer::default(),
+                    path: JsonPointer::default(),
                     message: v.to_string(),
                 }],
             },
             Rejection::Serde(_) => Self {
                 errors: vec![Error {
-                    path: JSONPointer::default(),
+                    path: JsonPointer::default(),
                     message: "invalid request".to_string(),
                 }],
             },
@@ -52,7 +80,7 @@ impl From<Rejection> for ErrorResponse {
                 errors: errors
                     .into_iter()
                     .map(|error| Error {
-                        path: error.instance_location().to_owned(),
+                        path: JsonPointer(error.instance_location().to_owned()),
                         message: error.error_description().to_string(),
                     })
                     .collect::<Vec<_>>(),
@@ -62,7 +90,7 @@ impl From<Rejection> for ErrorResponse {
                     .into_flat()
                     .into_iter()
                     .map(|error| Error {
-                        path: error.path,
+                        path: JsonPointer(error.path),
                         message: error.message,
                     })
                     .collect::<Vec<_>>(),
@@ -76,5 +104,28 @@ impl IntoResponse for Rejection {
         let mut res = axum::Json(ErrorResponse::from(self)).into_response();
         *res.status_mut() = StatusCode::BAD_REQUEST;
         res
+    }
+}
+
+#[cfg(feature = "aide")]
+mod impl_aide {
+    use super::*;
+
+    impl aide::OperationOutput for Rejection {
+        type Inner = Self;
+
+        fn operation_response(
+            ctx: &mut aide::gen::GenContext,
+            operation: &mut aide::openapi::Operation,
+        ) -> Option<aide::openapi::Response> {
+            axum::Json::<ErrorResponse>::operation_response(ctx, operation)
+        }
+
+        fn inferred_responses(
+            ctx: &mut aide::gen::GenContext,
+            operation: &mut aide::openapi::Operation,
+        ) -> Vec<(Option<u16>, aide::openapi::Response)> {
+            axum::Json::<ErrorResponse>::inferred_responses(ctx, operation)
+        }
     }
 }
