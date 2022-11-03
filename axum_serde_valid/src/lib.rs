6adc71b717321219cc3c2b5f19b8c2d1cc900a53
yassun4dev <yassun4dev@outlook.com>
2022-11-03 15:56:09 +0900

    Update: add trait.

--- axum_serde_valid/src/lib.rs
+++ axum_serde_valid/src/lib.rs
@@ -5,5 +5,7 @@ mod rejection;
 mod request;
 
 pub use json::Json;
+pub use jsonschema::JSONSchema as Schema;
 pub use query::Query;
 pub use rejection::{Error, ErrorResponse, Rejection};
+pub use serde_valid::Validate;
