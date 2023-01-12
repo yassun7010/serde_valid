8803fb4d90714aec131c91891e501b3d23d51402
Yasutanium <yassun4dev@outlook.com>
2023-01-12 23:31:26 +0900

    Add: README.md

--- scripts/generate_readme.sh
+++ scripts/generate_readme.sh
@@ -4,4 +4,5 @@ set -e
 
 cd "$(dirname "$0")/../serde_valid"
 
-cargo readme --no-indent-headings --no-title --no-license > README.md
+cargo readme --no-indent-headings --no-title --no-license >README.md
+cp README.md serde_valid/README.md
