6eaa23f7bab6210df82f17f0c126faaa17275b79
yassun7010 <yassun7010@outlook.com>
2024-01-14 18:10:12 +0900

    move: fluent to export.

--- serde_valid/src/features/fluent.rs
+++ serde_valid/src/features/fluent.rs
@@ -4,7 +4,6 @@ mod message;
 mod try_localize;
 
 pub use error::LocalizedError;
-pub use fluent::{FluentBundle, FluentError, FluentResource, FluentValue};
 pub use localize::Localize;
 pub use message::Message;
 pub use try_localize::TryLocalize;
