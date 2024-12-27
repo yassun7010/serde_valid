e574336de341f83e571d03fdb0bfc4ebc42aef1a
yassun7010 <yassun7010@outlook.com>
2024-12-27 22:39:29 +0900

    fix: ci.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,18 +2,20 @@
 
 set -e
 
-cd "$(dirname "$0")"/../xtask
+cd "$(dirname "$0")"/..
 
 cargo xtask update-tags
 
-cd ../crates/serde_valid_derive
+cd crates
+
+cd ../serde_valid_derive
 cargo publish
 
-cd ../crates/serde_valid_literal
+cd ../serde_valid_literal
 cargo publish
 
 # wait tarball package publishment
 sleep 20
 
-cd ../crates/serde_valid
+cd ../serde_valid
 cargo publish
